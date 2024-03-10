4a9b92ba04df872922055674f9e318a45ec18e51
procaffeinator <kritiarora2110@gmail.com>
2024-03-10 13:20:34 -0400

    code cleaning

--- main.js
+++ main.js
@@ -1,127 +1,117 @@
 loadPolarAreaChart();
-var nonDetoxLogos = [
-    ['Instagram', 'WhatsApp', 'Spotify'],
-    ['Instagram', 'WhatsApp', 'Spotify'],
-    ['Instagram', 'WhatsApp', 'Outlook'],
-    ['Instagram', 'WhatsApp', 'Spotify'],
-    ['Instagram', 'WhatsApp', 'Spotify'],
-    ['Instagram', 'WhatsApp', 'LinkedIn'],
-    ['Instagram', 'WhatsApp', 'Spotify'],
-    ['Instagram', 'WhatsApp', 'LinkedIn'],
-    ['Instagram', 'WhatsApp', 'Spotify'],
-    ['Instagram', 'WhatsApp', 'LinkedIn'],
-];
-
-var detoxLogos = [
-    ['WhatsApp', 'LinkedIn', 'Outlook'],
-    ['WhatsApp', 'Microsoft Teams', 'Spotify'],
-    ['WhatsApp', 'LinkedIn', 'Spotify'],
-    ['WhatsApp', 'Outlook', 'LinkedIn'],
-    ['WhatsApp', 'LinkedIn', 'Spotify'],
-    ['WhatsApp', 'LinkedIn', 'Spotify'],
-    ['WhatsApp', 'LinkedIn', 'Outlook'],
-    ['WhatsApp', 'LinkedIn', 'Outlook'],
-    ['Driver Start', 'WhatsApp', 'LinkedIn'],
-    ['WhatsApp', 'LinkedIn', 'Outlook'],
-];
 function loadPolarAreaChart() {
-    var nonDetoxData = [407, 413, 428, 415, 466, 345, 385, 429, 473, 374];
-    var detoxData = [232, 211, 245, 180, 209, 215, 299, 254, 337, 188];
-
-    // Sample data for menstrual cycle phases (color coding)
     var menstrualPhases = {
         "Luteal": "rgba(255, 165, 0, 0.6)",
         "Period": "rgba(255, 0, 0, 0.6)",
         "Follicular": "rgba(255, 255, 0, 0.6)",
         "Ovulation": "rgba(255, 192, 203, 0.6)"
     };
-    var menstrualDetox = ["Period", "Period", "Period", "Follicular", "Follicular", "Follicular", "Follicular", "Follicular", "Follicular", "Follicular"];
-    var menstrualNonDetox = ["Luteal", "Luteal", "Luteal", "Luteal", "Luteal", "Luteal", "Luteal", "Period", "Period", "Period"];
-
-    // Labels for the days
-    var labels = ["Day 0", "Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7", "Day 8", "Day 9"];
-
+    var nonDetoxApps = [];
+    var detoxApps = [];
+    var detoxData = [];
+    var nonDetoxData = [];
+    var labels = [];
+    var menstrualDetox = [];
+    var menstrualNonDetox = [];
     // Create the polar area chart
-    var ctx = document.getElementById('polarChart').getContext('2d');
+    fetch('data/dataset.csv')
+        .then(response => response.text())
+        .then(data => {
+            const lines = data.trim().split('\n');
+            lines.slice(1).forEach(row => { // Skip the header line
+                const rowData = row.split(';');
+                const apps = rowData[5] // Split the apps by comma and space
 
-    // Now, initialize the Chart object with the annotations included
-    var polarChart = new Chart(ctx, {
-        type: 'polarArea',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Detox Period',
-                data: detoxData,
-                backgroundColor: menstrualDetox.map(phase => menstrualPhases[phase]),
-            },
-            {
-                label: 'Non-Detox Period',
-                data: nonDetoxData,
-                backgroundColor: menstrualNonDetox.map(phase => menstrualPhases[phase]),
-            }
-            ]
-        },
-        options: {
-            scales: {
-                r: { // Change 'yAxis' to 'r'
-                    beginAtZero: true,
-                    title: {
-                        display: true,
-                        text: 'Screen Time (mins)' // Your y-axis label
-                    }
-                },
-                x: { // Change 'x' to 'xAxis'
-
-                    title: {
-                        display: true,
-                        text: 'Days' // Your x-axis label
-                    }
+                labels.push('Day ' + rowData[1]);
+                if (rowData[4] === 'Non-Detox') {
+                    nonDetoxApps.push(apps);
+                    nonDetoxData.push(rowData[2]);
+                    menstrualNonDetox.push(rowData[3]);
+                } else if (rowData[4] === 'Detox') {
+                    detoxApps.push(apps);
+                    detoxData.push(rowData[2]);
+                    menstrualDetox.push(rowData[3]);
                 }
-            },
-            plugins: {
-                legend: {
-                    display: false
+
+            });
+            var ctx = document.getElementById('polarChart').getContext('2d');
+
+            // Now, initialize the Chart object with the annotations included
+            var polarChart = new Chart(ctx, {
+                type: 'polarArea',
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        label: 'Detox Period',
+                        data: detoxData,
+                        backgroundColor: menstrualDetox.map(phase => menstrualPhases[phase]),
+                    },
+                    {
+                        label: 'Non-Detox Period',
+                        data: nonDetoxData,
+                        backgroundColor: menstrualNonDetox.map(phase => menstrualPhases[phase]),
+                    }
+                    ]
                 },
-                title: {
-                    display: false
-                },
-                tooltip: {
-                    enabled: true,
-                    callbacks: {
-                        afterLabel: function (context) {
-                            var apps = '';
-                            const datasetIndex = context.datasetIndex;
-                            const dataIndex = context.dataIndex;
-                            const topApps = datasetIndex === 0 ? detoxLogos[dataIndex] : nonDetoxLogos[dataIndex];
-                            topApps.forEach(app => {
-                                apps += `${app}, `;
-                            });
-                            apps = apps.slice(0, -2); // Remove the last comma and space
-                            return 'Top 3 apps: ' + apps;
+                options: {
+                    scales: {
+                        r: { // Change 'yAxis' to 'r'
+                            beginAtZero: true,
+                            title: {
+                                display: true,
+                                text: 'Screen Time (mins)' // Your y-axis label
+                            }
+                        },
+                        x: { // Change 'x' to 'xAxis'
+
+                            title: {
+                                display: true,
+                                text: 'Days' // Your x-axis label
+                            }
+                        }
+                    },
+                    plugins: {
+                        legend: {
+                            display: false
+                        },
+                        title: {
+                            display: false
+                        },
+                        tooltip: {
+                            enabled: true,
+                            callbacks: {
+                                afterLabel: function (context) {
+                                    const datasetIndex = context.datasetIndex;
+                                    const dataIndex = context.dataIndex;
+                                    var appData = datasetIndex === 0 ? detoxApps[dataIndex] : nonDetoxApps[dataIndex];
+                                    return 'Top 3 apps: ' + appData;
+                                }
+                            }
                         }
                     }
                 }
-            }
-        }
-    });
+            });
 
-    // Generate custom legend
-    var legend = document.getElementById('customLegend');
-    var legendTitle = document.createElement('div');
-    legendTitle.classList.add('legend-title');
-    legendTitle.textContent = 'Menstrual Cycle Phases';
-    legendTitle.style.marginRight = '10px';
-    legend.appendChild(legendTitle);
+            // Generate custom legend
+            var legend = document.getElementById('customLegend');
+            var legendTitle = document.createElement('div');
+            legendTitle.classList.add('legend-title');
+            legendTitle.textContent = 'Menstrual Cycle Phases';
+            legendTitle.style.marginRight = '10px';
+            legend.appendChild(legendTitle);
 
-    Object.keys(menstrualPhases).forEach(function (phase) {
-        var item = document.createElement('div');
-        item.classList.add('legend-item');
-        item.style.backgroundColor = menstrualPhases[phase];
-        var phaseName = document.createElement('span');
-        phaseName.textContent = phase;
-        item.appendChild(phaseName);
-        legend.appendChild(item);
-    });
+            Object.keys(menstrualPhases).forEach(function (phase) {
+                var item = document.createElement('div');
+                item.classList.add('legend-item');
+                item.style.backgroundColor = menstrualPhases[phase];
+                var phaseName = document.createElement('span');
+                phaseName.textContent = phase;
+                item.appendChild(phaseName);
+                legend.appendChild(item);
+            });
+        })
+        .catch(error => console.log('Error fetching CSV file:', error));
+
 }
 
 
